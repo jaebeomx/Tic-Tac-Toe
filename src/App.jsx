256e0ebf22438b7170fd7c68836dc568fd35caa6
jaebeom <sukjb80@gmail.com>
2024-01-12 17:19:18 +0900

    Player 기능추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
+
 function App() {
-  return <h1>Tic-Tac-Toe</h1>;
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="player1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          ></Player>
+          <Player
+            initialName="player2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          ></Player>
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        ></GameBoard>
+      </div>
+    </main>
+  );
 }
 
 export default App;
